987f9e8b2cb450c93967a0c2b21630196d0f6e6c
bonniee777 <a111221026@mail.shu.edu.tw>
2024-05-09 18:03:30 +0800

    20240509

---
+++ 20240509/ArrayAdaptSpinner/app/src/main/java/com/example/arrayadaptspinner/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.arrayadaptspinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.lang.ref.SoftReference;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner drink, temp;
+    private TextView txv;
+    private String[] tempSet1 = { "冰","去冰","溫"};
+    private String[] tempSet2 = { "冰","去冰"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txv = (TextView) findViewById(R.id.order);
+        temp = (Spinner) findViewById(R.id.temp);
+
+        drink = (Spinner) findViewById(R.id.drink);
+        drink.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String[] tempSet;
+                if (position == 3)
+                    tempSet = tempSet2;
+                else tempSet = tempSet1;
+
+                ArrayAdapter<String> tempAd =
+                        new ArrayAdapter<>( MainActivity.this,
+                                android.R.layout.simple_spinner_item,
+                                tempSet);
+            }
+        });
+    }
+}
